21a2692e3e1ef7b8dd605eb01e21a6cc1fd33966
Filip Skoczen <philipskoczen@gmail.com>
2025-06-30 21:38:10 +0200

    feat: Implement title of landing page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,15 +1,23 @@
 <script setup lang="ts">
-const count = ref(0);
 </script>
 
 <template>
-  <div class="min-h-screen grid place-items-center">
-    <UiButton @click="count++">
-      Increment
-    </UiButton>
-    <p>
-      {{ count }}
+  <div class="container mx-auto flex flex-col items-center mt-20 space-y-8">
+    <h1 class="capitalize text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-vibrant-from to-vibrant-pink text-balance text-center">
+      Join amazing sports events in minutes
+    </h1>
+    <p class="max-w-160 text-xl text-center text-balance text-muted-foreground">
+      Jumpyn makes it effortless to organize, manage, and join sports events.
+      From local tournaments to major competitions - we've got you covered.
     </p>
+    <div class="flex gap-8 flex-wrap">
+      <UiButton variant="vibrant">
+        Find event for you!
+      </UiButton>
+      <UiButton variant="outline">
+        Create event
+      </UiButton>
+    </div>
   </div>
 </template>
 
